b912b99a85cfdb3685fa21a7e7a1121262744f3b
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 04:28:26 +0300

    Update Student.java

--- src/main/java/com/epam/mjc/Student.java
+++ src/main/java/com/epam/mjc/Student.java
@@ -1,47 +1,50 @@
 package com.epam.mjc;
 
+public class Student {
+    private Long id;
+    private String name;
 
-import java.util.Arrays;
+    // Конструктор
+    public Student(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-public enum Student {
+    // Геттеры
+    public Long getId() {
+        return id;
+    }
 
-  MAX(1, "Max", 15),
-  KOLYA(2, "Kolya", 18),
-  SHAIL(3, "Shail", 30),
-  USHIR(4, "Ushir", 21),
-  OLEG(5, "Oleg", 33),
-  MIKITA(6, "Mikita", 20),
-  YULIA(7, "Yulia", 18),
-  STAS(8, "Stas", 25),
-  PETR(9, "Petr", 58),
-  ARTUR(10, "Artur", 26);
+    public String getName() {
+        return name;
+    }
 
-  private long id;
-  private String name;
-  private int age;
+    // Сеттеры (если нужно изменять данные студента)
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  Student(long id, String name, int age) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public static Student getValueOf(long id) {
-    return Arrays.stream(Student.values())
-        .filter(student -> id == student.getId())
-        .findFirst()
-        .orElse(null);
-  }
+    // Переопределим equals и hashCode для корректного сравнения объектов Student
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Student student = (Student) obj;
+        return id.equals(student.id);
+    }
 
-  public long getId() {
-    return id;
-  }
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 
-  public String getName() {
-    return name;
-  }
-
-  public int getAge() {
-    return age;
-  }
+    // Переопределим метод toString для удобного вывода данных студента
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}";
+    }
 }
